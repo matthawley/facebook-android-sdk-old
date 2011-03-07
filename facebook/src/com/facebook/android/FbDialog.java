77903bb0edffcfcad56a2e7a3895dbf4007d95b7
matthawley <matt@eworldui.net>
2011-03-07 08:22:50 -0800

    Adding in changes to dialog that were lost

--- facebook/src/com/facebook/android/FbDialog.java
+++ facebook/src/com/facebook/android/FbDialog.java
@@ -17,46 +17,37 @@
 package com.facebook.android;
 
 import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
-import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.Window;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.ViewAnimator;
 
 import com.facebook.android.Facebook.DialogListener;
 
 public class FbDialog extends Dialog {
 
-    static final int FB_BLUE = 0xFF6D84B4;
     static final float[] DIMENSIONS_LANDSCAPE = {460, 260};
     static final float[] DIMENSIONS_PORTRAIT = {280, 420};
-    static final FrameLayout.LayoutParams FILL =
-        new FrameLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
-                         ViewGroup.LayoutParams.FILL_PARENT);
-    static final int MARGIN = 4;
-    static final int PADDING = 2;
     static final String DISPLAY_STRING = "touch";
-    static final String FB_ICON = "icon.png";
 
     private String mUrl;
     private DialogListener mListener;
-    private ProgressDialog mSpinner;
     private WebView mWebView;
-    private LinearLayout mContent;
+    private View mContent;
     private TextView mTitle;
+    private ViewAnimator switcher;
 
     public FbDialog(Context context, String url, DialogListener listener) {
         super(context);
@@ -67,49 +58,34 @@ public class FbDialog extends Dialog {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mSpinner = new ProgressDialog(getContext());
-        mSpinner.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        mSpinner.setMessage("Loading...");
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        mContent = inflater.inflate(R.layout.facebook_dialog, null);
 
-        mContent = new LinearLayout(getContext());
-        mContent.setOrientation(LinearLayout.VERTICAL);
-        setUpTitle();
-        setUpWebView();
         Display display = getWindow().getWindowManager().getDefaultDisplay();
         final float scale = getContext().getResources().getDisplayMetrics().density;
-        float[] dimensions =
-            (display.getWidth() < display.getHeight())
-                    ? DIMENSIONS_PORTRAIT : DIMENSIONS_LANDSCAPE;
+        float[] dimensions = (display.getWidth() < display.getHeight())
+                    			? DIMENSIONS_PORTRAIT 
+                    			: DIMENSIONS_LANDSCAPE;
+        
         addContentView(mContent, new FrameLayout.LayoutParams(
                 (int) (dimensions[0] * scale + 0.5f),
                 (int) (dimensions[1] * scale + 0.5f)));
-    }
-
-    private void setUpTitle() {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        Drawable icon = getContext().getResources().getDrawable(
-                R.drawable.facebook_icon);
-        mTitle = new TextView(getContext());
-        mTitle.setText("Facebook");
-        mTitle.setTextColor(Color.WHITE);
-        mTitle.setTypeface(Typeface.DEFAULT_BOLD);
-        mTitle.setBackgroundColor(FB_BLUE);
-        mTitle.setPadding(MARGIN + PADDING, MARGIN, MARGIN, MARGIN);
-        mTitle.setCompoundDrawablePadding(MARGIN + PADDING);
-        mTitle.setCompoundDrawablesWithIntrinsicBounds(
-                icon, null, null, null);
-        mContent.addView(mTitle);
-    }
-
-    private void setUpWebView() {
-        mWebView = new WebView(getContext());
-        mWebView.setVerticalScrollBarEnabled(false);
-        mWebView.setHorizontalScrollBarEnabled(false);
+        
+        switcher = (ViewAnimator) findViewById(R.id.switcher);
+        mTitle = (TextView) findViewById(R.id.title);
+        mWebView = (WebView) findViewById(R.id.web);
+        
         mWebView.setWebViewClient(new FbDialog.FbWebViewClient());
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.loadUrl(mUrl);
-        mWebView.setLayoutParams(FILL);
-        mContent.addView(mWebView);
+        
+        setOnCancelListener(new OnCancelListener() {
+        	public void onCancel(DialogInterface dialog) {
+        		mListener.onCancel();
+        	}
+        });
     }
 
     private class FbWebViewClient extends WebViewClient {
@@ -143,18 +119,16 @@ public class FbDialog extends Dialog {
             } else if (url.contains(DISPLAY_STRING)) {
                 return false;
             }
+            
             // launch non-dialog URLs in a full browser
-            getContext().startActivity(
-                    new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+            getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
             return true;
         }
 
         @Override
-        public void onReceivedError(WebView view, int errorCode,
-                String description, String failingUrl) {
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
             super.onReceivedError(view, errorCode, description, failingUrl);
-            mListener.onError(
-                    new DialogError(description, errorCode, failingUrl));
+            mListener.onError(new DialogError(description, errorCode, failingUrl));
             FbDialog.this.dismiss();
         }
 
@@ -162,7 +136,8 @@ public class FbDialog extends Dialog {
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
             Log.d("Facebook-WebView", "Webview loading URL: " + url);
             super.onPageStarted(view, url, favicon);
-            mSpinner.show();
+            if (switcher.getDisplayedChild() != 0)
+            	switcher.setDisplayedChild(0);
         }
 
         @Override
@@ -172,7 +147,9 @@ public class FbDialog extends Dialog {
             if (title != null && title.length() > 0) {
                 mTitle.setText(title);
             }
-            mSpinner.dismiss();
+            
+            switcher.setDisplayedChild(1);
+            mWebView.requestFocus();
         }
 
     }
